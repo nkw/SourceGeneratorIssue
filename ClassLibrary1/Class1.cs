9c4333dd962af3d9b6b39f7521aa32df407f47e5
nkw <nickwang@gmail.com>
2023-12-25 10:28:16 -0500

    init

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary1;
+
+public static partial class Class1
+{
+    public static string GetS(string s)
+    {
+        var x = GetS2(s);
+
+        return x ?? throw new InvalidOperationException();
+    }
+
+    private static partial string? GetS2(string s);
+}
---
+++ SourceGenerators/Class1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Text;
+
+namespace ClassLibrary1;
+
+[Generator]
+public class SourceGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        System.Console.WriteLine("Test....");
+
+        var sb = new StringBuilder();
+        sb.AppendLine("namespace ClassLibrary1;");
+        sb.AppendLine("public static partial class Class1");
+        sb.AppendLine("{");
+        sb.AppendLine("    private static partial string? GetS2(string s)");
+        sb.AppendLine("    {");
+        sb.AppendLine("        return \"Test\"");
+        sb.AppendLine("    }");
+        sb.AppendLine("}");
+
+        context.AddSource("Class1.g.cs", sb.ToString());
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+}
